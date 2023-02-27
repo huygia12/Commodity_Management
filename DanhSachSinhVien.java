e715cb92f5fda72cec7fc87aa97a52ed252f322a
huygia12 <huynguyengia1203@gmail.com>
2023-02-27 20:08:45 +0700

    Update DanhSach

--- DanhSachSinhVien.java
+++ DanhSachSinhVien.java
@@ -16,7 +16,9 @@ import java.util.Scanner;
  * @author FPTSHOP
  */
 public class DanhSachSinhVien {
-
+    public static void temp(){
+        
+    }
     public static void main(String[] args) throws FileNotFoundException {
         List<SinhVien> list = new ArrayList<SinhVien>();
         Function f = new Function(list);
